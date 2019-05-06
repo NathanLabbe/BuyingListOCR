781b3b32a5059ef3b6036faf8a816f38f06e6b20
irex13 <robsdg13@gmail.com>
2019-05-06 12:04:43 +0200

    Ajout activité appareil photo

---
+++ app/src/main/java/com/example/buyinglistocr/controller/AppareilPhoto.java
@@ -0,0 +1,13 @@
+package com.example.buyinglistocr.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppareilPhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appareil_photo);
+    }
+}
