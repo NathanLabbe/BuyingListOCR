067479e74338849b38154517d195a31d18a96946
irex13 <robsdg13@gmail.com>
2019-05-06 11:25:45 +0200

    Test

--- app/src/main/java/com/example/buyinglistocr/MainActivity.java
+++ app/src/main/java/com/example/buyinglistocr/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.buyinglistocr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mGreetingText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGreetingText.setText("Bonjour");
     }
     //testf
 }
