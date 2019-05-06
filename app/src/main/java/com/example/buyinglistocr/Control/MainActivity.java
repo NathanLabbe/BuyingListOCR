2ba2b70b25ebcbf028935ed613e9ac82d86d911c
nathan <nathanlab1997@gmail.com>
2019-05-06 11:28:47 +0200

    Origin2

--- app/src/main/java/com/example/buyinglistocr/Control/MainActivity.java
+++ app/src/main/java/com/example/buyinglistocr/Control/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.buyinglistocr;
+package com.example.buyinglistocr.Control;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.example.buyinglistocr.R;
 
 public class MainActivity extends AppCompatActivity {
 
