ba193c922b64c4678131ee3a1ec063380505ae5d
bastienricard <bastien.ricard@hotmail.fr>
2019-05-07 15:57:01 +0200

    Ajout de la vue d'une liste.

---
+++ app/src/main/java/com/example/buyinglistocr/controller/ListView.java
@@ -0,0 +1,15 @@
+package com.example.buyinglistocr.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.buyinglistocr.R;
+
+public class ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+}
