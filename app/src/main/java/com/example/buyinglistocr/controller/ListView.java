1b450e4d6f31a36e955156f781198a0d466fa2ca
irex13 <robsdg13@gmail.com>
2019-05-14 15:25:40 +0200

    Apparreil photo fusionné avec ListView

--- app/src/main/java/com/example/buyinglistocr/controller/ListView.java
+++ app/src/main/java/com/example/buyinglistocr/controller/ListView.java
@@ -1,19 +1,47 @@
 package com.example.buyinglistocr.controller;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.buyinglistocr.BuildConfig;
 import com.example.buyinglistocr.R;
+import com.googlecode.leptonica.android.WriteFile;
+import com.googlecode.tesseract.android.TessBaseAPI;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 
 public class ListView extends AppCompatActivity {
@@ -24,13 +52,25 @@ public class ListView extends AppCompatActivity {
     //REFERENCE
     Button addNewItem;
     android.widget.ListView listView;
-    Button test;
+
 
     //LISTE DE STRING QUI SERVIRA COMME LISTE D'ITEMS
     ArrayList<String> listItems = new ArrayList<String>();
 
     //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
     ArrayAdapter<String> adapter;
+
+    /**
+     * Partie photo
+     */
+
+    public static final String TESS_DATA = "/tessdata";
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/Tess";
+    private TessBaseAPI tessBaseAPI;
+    private Uri outputFileDir;
+    private String mCurrentPhotoPath;
+    HashMap<String, ArrayList<String>> listProduit;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -43,7 +83,7 @@ public class ListView extends AppCompatActivity {
         //AFFICHAGE DES REFERENCES
         addNewItem = (Button) findViewById(R.id.activity_main_activity_add_new_item);
         listView = (android.widget.ListView) findViewById(R.id.activity_main_list_view);
-        test = (Button) findViewById(R.id.button2);
+
 
 
         //AFFICHAGE DES ELEMENTS DE NOTRE LISTE
@@ -68,6 +108,15 @@ public class ListView extends AppCompatActivity {
             }
         });
         */
+        final Activity activity = this;
+        checkPermission();
+        this.findViewById(R.id.buttonTakePhoto).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermission();
+                dispatchTakePictureIntent();
+            }
+        });
     }
 
     private void viewData() {
@@ -89,5 +138,159 @@ public class ListView extends AppCompatActivity {
     public void addItemsView(View v) {
         Intent AddElementIntent = new Intent(ListView.this, AddElement.class);
         startActivity(AddElementIntent);
+    }
+
+
+    /**
+     * PARTIE PHOTO
+     */
+    //Gestion Des Permissions
+    private void checkPermission() {
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 122);
+        }
+
+
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        BuildConfig.APPLICATION_ID + ".provider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, 1024);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1024) {
+            if (resultCode == Activity.RESULT_OK) {
+                prepareTessData();
+                startOCR(outputFileDir);
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(getApplicationContext(), "Result canceled.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Activity result failed.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void prepareTessData() {
+        try {
+            File dir = getExternalFilesDir(TESS_DATA);
+            if (!dir.exists()) {
+                if (!dir.mkdir()) {
+                    Toast.makeText(getApplicationContext(), "The folder " + dir.getPath() + "was not created", Toast.LENGTH_SHORT).show();
+                }
+            }
+            String fileList[] = getAssets().list("");
+            for (String fileName : fileList) {
+                String pathToDataFile = dir + "/" + fileName;
+                if (!(new File(pathToDataFile)).exists()) {
+                    InputStream in = getAssets().open(fileName);
+                    OutputStream out = new FileOutputStream(pathToDataFile);
+                    byte[] buff = new byte[1024];
+                    int len;
+                    while ((len = in.read(buff)) > 0) {
+                        out.write(buff, 0, len);
+                    }
+                    in.close();
+                    out.close();
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    private void startOCR(Uri imageUri) {
+        try {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = false;
+            options.inSampleSize = 6;
+            System.out.println(options.toString());
+
+            Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, options);
+            String result = this.getText(bitmap);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    //ORIENTATION BITMAP
+    public Bitmap rotateBitmap(Bitmap original, float degrees) {
+        Bitmap bOutput;
+        float degreees = 90;//rotation degree
+        Matrix matrix = new Matrix();
+        matrix.setRotate(degreees);
+        bOutput = Bitmap.createBitmap(original, 0, 0, original.getWidth(), original.getHeight(), matrix, true);
+        return bOutput;
+    }
+
+    private String getText(Bitmap bitmap) {
+        try {
+            tessBaseAPI = new TessBaseAPI();
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+        String dataPath = getExternalFilesDir("/").getPath() + "/";
+        tessBaseAPI.init(dataPath, "fra",TessBaseAPI.OEM_TESSERACT_ONLY);
+        tessBaseAPI.setVariable(TessBaseAPI.VAR_CHAR_WHITELIST, "aàAbBcçCdDeEéèêfFgGhHiIjJkKlLmMnNoôOpPqQrRsStTuùUvVwWxXyYzZ1234567890\',.;+-_%/ ");
+        /**
+         * Selon le téléphone commentez
+         */
+        //tessBaseAPI.setImage(rotateBitmap(bitmap, 90));
+        tessBaseAPI.setImage(bitmap);
+
+
+        String retStr = "No result";
+        try {
+            retStr = tessBaseAPI.getUTF8Text();
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+        Bitmap bitmapfinal = WriteFile.writeBitmap(tessBaseAPI.getThresholdedImage());
+
+        tessBaseAPI.end();
+        AnalyseData test = new AnalyseData(retStr);
+        //retStr = test.clean(retStr);
+        System.out.println(retStr);
+        return retStr;
     }
 }
