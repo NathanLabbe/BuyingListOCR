1b95e1e45566fe6b7800e9685b694abb11b12ad6
chiLinh0412 <chilinh0412@gmail.com>
2019-06-11 12:42:06 +0200

    Correspondence et CorrespondenceDAO

---
+++ app/src/main/java/com/example/buyinglistocr/model/CorrespondenceDAO.java
@@ -0,0 +1,307 @@
+package com.example.buyinglistocr.model;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Chi Linh on 2019--2019-06-11--12:29.
+ */
+public class CorrespondenceDAO {
+    public Context context;
+
+    public CorrespondenceDAO(Context context) {
+        this.context = context;
+    }
+
+    public long add (final Correspondence correspondence) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://51.83.70.93/android/BuyingListOCR/ListIndex.php",
+
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            Toast.makeText(context, jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+
+                        } catch(JSONException e) {
+
+                            e.printStackTrace();
+
+                        }
+
+
+                    }
+
+                },
+
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(context, error.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+
+        ) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("tag","add");
+                params.put("name", ""+ correspondence.getName());
+                return params;
+
+            }
+
+        };
+
+        RequestHandler.getInstance(context).addToRequestQueue(stringRequest);
+
+        return 0;
+
+    }
+
+    public void delete(final long id) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://51.83.70.93/android/BuyingListOCR/ListIndex.php",
+
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            Toast.makeText(context, jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+
+                        } catch(JSONException e) {
+
+                            e.printStackTrace();
+
+                        }
+
+
+                    }
+
+                },
+
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(context, error.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+
+        ) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("tag","delete");
+                params.put("id", ""+id);
+                return params;
+
+            }
+
+        };
+
+        RequestHandler.getInstance(context).addToRequestQueue(stringRequest);
+
+
+
+    }
+
+    public void update(final Correspondence correspondence) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://51.83.70.93/android/BuyingListOCR/ListIndex.php",
+
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            Toast.makeText(context, jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+
+                        } catch(JSONException e) {
+
+                            e.printStackTrace();
+
+                        }
+
+
+                    }
+
+                },
+
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(context, error.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+
+        ) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("tag","update");
+                params.put("name", ""+ correspondence.getName());
+                return params;
+
+            }
+
+        };
+
+        RequestHandler.getInstance(context).addToRequestQueue(stringRequest);
+
+
+    }
+
+    public ArrayList<Correspondence> get(final long listKey) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://51.83.70.93/android/BuyingListOCR/ListIndex.php",
+
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            Toast.makeText(context, jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+
+                        } catch(JSONException e) {
+
+                            e.printStackTrace();
+
+                        }
+
+
+                    }
+
+                },
+
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(context, error.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+
+        ) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("tag","get");
+                params.put("listKey", "" + listKey);
+                return params;
+
+            }
+
+        };
+
+        RequestHandler.getInstance(context).addToRequestQueue(stringRequest);
+
+        return null;
+    }
+
+    public Correspondence getCorrespondence (final long id) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://51.83.70.93/android/BuyingListOCR/ListIndex.php",
+
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            Toast.makeText(context, jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+
+                        } catch(JSONException e) {
+
+                            e.printStackTrace();
+
+                        }
+
+
+                    }
+
+                },
+
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(context, error.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+
+        ) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("tag","getCorrespondence");
+                params.put("id", ""+ id);
+                return params;
+
+            }
+
+        };
+
+        RequestHandler.getInstance(context).addToRequestQueue(stringRequest);
+
+        return null;
+
+    }
+
+}
